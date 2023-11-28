b395aa4e4f4e4cf3c20b82f61607f1f1da857c0f
Micah Geisel <micah@botandrose.com>
2023-11-28 12:11:36 -0600

    get dev hotload cycle working again.

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,12 +4,24 @@ export const config: Config = {
   namespace: 'bard-file',
   outputTargets: [
     {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
       type: 'dist-custom-elements',
+      dir: 'dist/components',
       customElementsExportBehavior: 'single-export-module',
       isPrimaryPackageOutputTarget: true,
     },
     {
       type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      copy: [
+        { src: 'images' }
+      ]
+      serviceWorker: null, // disable service workers
     },
   ],
   testing: {
