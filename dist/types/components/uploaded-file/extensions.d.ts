3cae07dbb05f60806e3c6c13a3fa59713ed4a5f8
Micah Geisel <micah@botandrose.com>
2023-12-18 13:58:07 -0600

    add 3gpp to list of accepted video files.

---
+++ dist/types/components/uploaded-file/extensions.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    image: string[];
+    video: string[];
+    pdf: string[];
+    getFileType: (filename: any) => "unknown" | "pdf" | "video" | "image";
+};
+export default _default;
